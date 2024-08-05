f6a1243f90c5025001fb0635ec6a56cd2bd5192c
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-08-05 12:05:40 +0200

    test su modulo logging e request.GET (progetto prova django)

--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/prova/urls.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/prova/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    re_path(r"^$|^/$|^home/$", views.home_page, name='homepage'),
+    path("elencoparametri/",views.elenca_params, name='params'),
     path('admin/', admin.site.urls),
 ]
--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/prova/views.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/prova/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 
+import logging
+logger = logging.getLogger(__name__)
+
 def home(request):
-  return HttpResponse("Hello world! Questa è la home page.")
+    return HttpResponse("Hello world! Questa è la home page.")
+
+def home_page(request):
+    response = f"Benvenuto nella homepage, {str(request.user)}"
+  
+    if not request.user.is_authenticated:
+        logger.warning(f'{str(request.user)} non è autenticato!')
+
+    return HttpResponse(response)
+    
+def elenca_params(request):
+    response=''
+    for k in request.GET:
+        response += request.GET[k]
+
+    return HttpResponse(response)
