7eb796554dd9710c67e3444863ff7e6fe9d5f116
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-08-10 12:48:36 +0200

    aggiunte view per interagire con Persona (django prova)

--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/urls.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='soci'
 
 urlpatterns = [
+        path('persona-list', views.PersonaList.as_view(), name='persona-list'),
         path('persona-create', views.PersonaCreate.as_view(), name='persona-create'),
+        path('persona-delete/<int:pk>', views.PersonaDelete.as_view(), name='persona-delete')
         ]
--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/views.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView
 from .models import *
 
 # Create your views here.
+
+class PersonaList(ListView):
+    model = Persona
+    template_name = "soci/persona_list.html"
 
 class PersonaCreate(CreateView):
     model = Persona
@@ -11,3 +16,7 @@ class PersonaCreate(CreateView):
     template_name = 'soci/persona_create.html'
     success_url = reverse_lazy('soci:persona-list')
   
+class PersonaDelete(DeleteView):
+    model = Persona
+    template_name = 'soci/persona_delete.html'
+    success_url = reverse_lazy('soci:persona-list')
