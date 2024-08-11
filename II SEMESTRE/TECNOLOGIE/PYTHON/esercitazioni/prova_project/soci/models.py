131cfc7abf094c6319a5f099086419709cc6d8de
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-08-11 08:14:26 +0200

    setup amministrazione modelli django prova

--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/models.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/prova_project/soci/models.py
@@ -12,4 +12,6 @@ class Persona(models.Model):
     nome = models.CharField(max_length=50)
     cognome = models.CharField(max_length=50)
     ruolo = models.ForeignKey(Ruolo, on_delete=models.PROTECT, related_name='persone')
-
+    
+    class Meta:
+        verbose_name_plural = 'Persone'
