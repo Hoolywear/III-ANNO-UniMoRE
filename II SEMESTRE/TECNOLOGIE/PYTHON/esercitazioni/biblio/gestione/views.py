8570def6506588476eb210d1634ba91bf005364d
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-08-18 12:42:40 +0200

    aggiunta view mattoni (django biblio)

--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/biblio/gestione/views.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/biblio/gestione/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Libro
 
-# Create your views here.
 
-def home(request):
-    return HttpResponse("Hello world!")
+# Create your views here.
 
 def lista_libri(request):
     templ = 'gestione/listalibri.html'
 
     ctx = {
-        "title" : "Lista di libri",
-        "listalibri" : Libro.objects.all()
+        "title": "Lista di libri",
+        "listalibri": Libro.objects.all()
+    }
+
+    return render(request, templ, ctx)
+
+
+MATTONI_THRESHOLD = 300
+
+
+def mattoni(request):
+    templ = 'gestione/listalibri.html'
+
+    lista_filtrata = Libro.objects.all().filter(pagine__gte=MATTONI_THRESHOLD)
+
+    ctx = {
+        "title": "Lista di mattoni",
+        "listalibri": lista_filtrata
     }
 
     return render(request, templ, ctx)
