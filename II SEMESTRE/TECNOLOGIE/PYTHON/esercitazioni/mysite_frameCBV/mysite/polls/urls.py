760b0befc1fc5e2fb9a6888891b65d22ee58b70f
Iacopo Ruzzier <iacopo@alessandroruzzier.it>
2024-08-22 12:02:45 +0200

    ModelForm fine pdf 11a (django mysite_frameCBV)

--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/mysite_frameCBV/mysite/polls/urls.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/mysite_frameCBV/mysite/polls/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('', IndexViewList.as_view(), name='index'),
     path('<pk>/detail/', DetailQuestion.as_view(), name='detail'),
     path('search/', search, name='search'),
-    path('searchresults/<str:sstring>/<str:where>/', SearchResultsList.as_view(), name='searchresults')
-
-
+    path('searchresults/<str:sstring>/<str:where>/', SearchResultsList.as_view(), name='searchresults'),
+    path('vote/<int:pk>/', vote, name='vote'),
+    path('votecasted/<int:pk>/<str:answer>', VoteCastedDetail.as_view(), name='votecasted'),
+    path('createquestion/', CreateQuestionView.as_view(), name='createquestion'),
+    path('createchoice/', CreateChoiceView.as_view(), name='createchoice'),
 ]
--- II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/mysite_frameCBV/mysite/polls/views.py
+++ II SEMESTRE/TECNOLOGIE/PYTHON/esercitazioni/mysite_frameCBV/mysite/polls/views.py
@@ -51,3 +51,54 @@ class SearchResultsList(ListView):
             for c in qc:
                 qq |= Question.objects.filter(pk=c.question_id)
         return qq
+
+
+def vote(request, pk):
+    if request.method == "POST":
+        form = VoteForm(data=request.POST, pk=pk)
+        if form.is_valid():
+            answer = form.cleaned_data.get("answer")
+            return redirect("polls:votecasted", pk, answer.choice_text)
+    else:
+        q = get_object_or_404(Question, pk=pk)
+        form = VoteForm(pk=pk)
+        return render(request, template_name="polls/vote.html", context={"form": form, "question": q})
+
+
+class VoteCastedDetail(DetailView):
+    model = Question
+    template_name = "polls/votecasted.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        answer = self.request.resolver_match.kwargs["answer"]
+        ctx["answer"] = answer
+        correct = ctx["object"].choices.all().get(is_correct=True)
+        if answer in correct.choice_text:
+            ctx["message"] = "Right Answer!"
+        else:
+            ctx["message"] = "Wrong Answer! " + " The right answer was " + str(correct.choice_text)
+
+        try:
+            c = ctx["object"].choices.all().get(choice_text=answer)
+            c.votes += 1
+            c.save()
+        except Exception as e:
+            print("Impossible to update vote value " + str(e))
+        return ctx
+
+
+class CreateQuestionView(CreateView):
+    template_name = "polls/createentry.html"
+    form_class = CreateQuestionForm
+    success_url = reverse_lazy("polls:index")
+
+
+class CreateChoiceView(CreateView):
+    template_name = "polls/createentry.html"
+    form_class = CreateChoiceForm
+
+    def get_success_url(self):
+        ctx = self.get_context_data()
+        pk = ctx["object"].question.pk
+        return reverse("polls:detail", kwargs={"pk": pk})
